cae74a363590f3fffe7851567160838d53741893
mrMcAko <alechacin08@gmail.com>
2026-01-14 16:58:49 -0600

    feat: Creating directory Users and moving User Entity to this. Also create UserStatus Enum, to have statos on Users

---
+++ Venekia.Domain/Entities/Users/User.cs
@@ -0,0 +1,106 @@
+namespace Venekia.Domain.Entities.Users
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+        public string FirstName { get; private set; } = null!;
+        public string LastName { get; private set; } = null!;
+        public string Email { get; private set; } = null!;
+        public string PasswordHash { get; private set; } = null!;
+        public string? PhoneNumber { get; private set; }
+        public string? Address { get; private set; }
+
+        public UserStatus Status { get; private set; } = UserStatus.Active;
+
+        public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; private set; } = DateTime.UtcNow;
+        public DateTime? DeactivatedAt { get; private set; }
+
+        private User() { }
+        public User(string firstName, string lastName, string email, string passwordHash, string? phoneNumber, string? address)
+        {
+            Id = Guid.NewGuid();
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            PasswordHash = passwordHash;
+            PhoneNumber = phoneNumber;
+            Address = address;
+            Status = UserStatus.Active;
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public void UpdateFirstName(string firstName)
+        {
+            if (string.IsNullOrWhiteSpace(firstName))
+                throw new Exception("First Name is required");
+
+            FirstName = firstName;
+            Touch();
+        }
+
+        public void UpdateLastName(string lastName)
+        {
+            if (string.IsNullOrWhiteSpace(lastName))
+                throw new Exception("First Name is required");
+
+            LastName = lastName;
+            Touch();
+        }
+
+        public void UpdatePhoneNumber(string? phoneNumber) {
+            PhoneNumber = phoneNumber;
+            Touch();
+        }
+        public void UpdateAddress(string? address) {
+           Address = address;
+           Touch();
+        } 
+        public void UpdatePassword(string newPassword)
+        {
+            PasswordHash = newPassword;
+            Touch();
+        }
+
+        public bool CanLogin(DateTime now)
+        {
+            return IsActive();
+        }
+
+        public void ChangeStatus(UserStatus status)
+        {
+            Status = status;
+
+            if (status == UserStatus.Inactive)
+            {
+                DeactivatedAt = DateTime.UtcNow;
+            }
+            else
+            {
+                DeactivatedAt = null;
+            }
+        }
+
+        //public void CompletePasswordChange()
+        //{
+        //    if (Status != UserStatus.ChangePassword)
+        //        throw new InvalidOperationException("User status is not set to ChangePassword");
+        //    Status = UserStatus.Active;
+        //}
+        public bool IsActive()
+        {
+            return Status == UserStatus.Active;
+        }
+        public void Deactivate()
+        {
+            Status = UserStatus.Inactive;
+            DeactivatedAt = DateTime.UtcNow;
+        }
+        private void Touch()
+        {
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+    }
+}
---
+++ Venekia.Domain/Entities/Users/UserStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Venekia.Domain.Entities.Users
+{
+    public enum UserStatus
+    {
+        Active = 1,
+        Inactive = 2,
+        //ChangePassword = 3
+    }
+}
