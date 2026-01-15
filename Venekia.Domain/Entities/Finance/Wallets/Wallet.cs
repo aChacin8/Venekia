83962e33bcb1224b957dbab538eaab454451628a
mrMcAko <alechacin08@gmail.com>
2026-01-15 14:06:21 -0600

    feat: Creating the wallet entity and the enum WalletStatus

---
+++ Venekia.Domain/Entities/Finance/Wallets/Wallet.cs
@@ -0,0 +1,29 @@
+﻿using Venekia.Domain.Entities.Users;
+
+namespace Venekia.Domain.Entities.Finance.Wallets
+{
+    public class Wallet
+    {
+        public Guid Id { get; private set; }
+        public Guid UserId { get; private set; }
+        public string Currency { get; set; } = null!;
+        public decimal Balance { get; private set; } = 0.00m;
+        public WalletStatus Status { get; set; } = WalletStatus.active;
+        public User User { get; private set; } = null!;
+        public DateTime CreatedAt { get; private set; }
+        public DateTime UpdatedAt { get; private set; }
+
+        public Wallet() {}
+
+        public Wallet(Guid userId, string currency)
+        {
+            Id = Guid.NewGuid();
+            UserId = userId;
+            Currency = currency;
+            Balance = 0m;
+            Status = WalletStatus.active;
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = DateTime.UtcNow;
+        }
+    }
+}
---
+++ Venekia.Domain/Entities/Finance/Wallets/WalletStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Venekia.Domain.Entities.Finance.Wallets
+{
+    public enum WalletStatus
+    {
+        active = 1,
+        inactive = 2,
+        suspended = 3
+    }
+}
