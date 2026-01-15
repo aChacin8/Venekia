b928a158ca4bed1091fe94a46ea72de690ee6e96
mrMcAko <alechacin08@gmail.com>
2026-01-15 14:07:33 -0600

    feat: Creating the WalletConfiguration using the EfCore Model, and creating the columns and attributes

---
+++ Venekia.Infrastructure/Data/Finance/WalletConfig.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Venekia.Domain.Entities.Finance.Wallets;
+
+namespace Venekia.Infrastructure.Data.Finance
+{
+    public class WalletConfig : IEntityTypeConfiguration<Wallet>
+    {
+        public void Configure(EntityTypeBuilder<Wallet> entity)
+        {
+            entity.ToTable("Wallets", "dbo");
+            entity.HasKey(w => w.Id);
+            entity.Property(w => w.Id).HasColumnName("Id").ValueGeneratedNever();
+            entity.Property(w => w.UserId).HasColumnName("UserId").IsRequired();
+            entity.HasOne(w => w.User).WithOne(u => u.Wallet).HasForeignKey<Wallet>(w => w.UserId).OnDelete(DeleteBehavior.Cascade);
+
+            entity.Property(w => w.Currency).HasColumnName("Currency").IsRequired();
+            entity.Property(w => w.Balance).HasColumnName("Balance").IsRequired().HasDefaultValue(0.00);
+            entity.Property(w => w.Status).HasColumnName("Status").IsRequired();
+            entity.Property(u => u.CreatedAt).HasColumnName("CreatedAt").HasDefaultValueSql("GETUTCDATE()");
+            entity.Property(u => u.UpdatedAt).HasColumnName("UpdatedAt").HasDefaultValueSql("GETUTCDATE()");
+        }
+    }
+}
+
