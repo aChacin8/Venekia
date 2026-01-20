8847ea4e92a199f2206432746808704372ca7f61
mrMcAko <alechacin08@gmail.com>
2026-01-19 21:17:12 -0600

    feat: Renaming CreateWalletDto to CreateWalletAsync

--- Venekia.Application/Interfaces/Finance/Wallets/IWalletService.cs
+++ Venekia.Application/Interfaces/Finance/Wallets/IWalletService.cs
@@ -4,7 +4,7 @@ namespace Venekia.Application.Interfaces.Finance.Wallets
 {
     public interface IWalletService
     {
-        Task<WalletResponseDto> CreateWalletDto(Guid userId, CreateWalletDto createWalletDto);
+        Task<WalletResponseDto> CreateWalletAsync(Guid userId, CreateWalletDto createWalletDto);
         Task<List<WalletResponseDto>> GetWalletByUserAsync(Guid userId);
         Task CreditAsync(Guid userId, CreditWalletDto creditWalletDto);
         Task DebitAsync(Guid userId, DebitWalletDto debitWalletDto);
