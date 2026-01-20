9aad2f9a44cdf6ab300abd17ded91281e07c924f
mrMcAko <alechacin08@gmail.com>
2026-01-19 20:40:37 -0600

    feat: Creating Wallet response dto

---
+++ Venekia.Application/DTOs/Finance/Wallet/WalletResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Venekia.Application.DTOs.Finance.Wallet
+{
+    public class WalletResponseDto
+    {
+        Guid Id { get; set; }
+        decimal Balance { get; set; }
+        string Currency { get; set; } = null!;
+        string Status { get; set; } = null!;
+    }
+}
