9cface6f5b0c040cbcc25a86a1c1b02e1088f2f5
mrMcAko <alechacin08@gmail.com>
2026-01-19 19:49:12 -0600

    feat: Refactor to the wallet configuration columns. Adding MaxLength to Currency and a correct default value to Balance.

--- Venekia.Infrastructure/Data/Finance/WalletConfig.cs
+++ Venekia.Infrastructure/Data/Finance/WalletConfig.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Venekia.Domain.Entities.Finance.Wallets;
+using Venekia.Domain.Entities.Users;
 
 namespace Venekia.Infrastructure.Data.Finance
 {
@@ -12,10 +13,10 @@ namespace Venekia.Infrastructure.Data.Finance
             entity.HasKey(w => w.Id);
             entity.Property(w => w.Id).HasColumnName("Id").ValueGeneratedNever();
             entity.Property(w => w.UserId).HasColumnName("UserId").IsRequired();
-            entity.HasOne(w => w.User).WithOne(u => u.Wallet).HasForeignKey<Wallet>(w => w.UserId).OnDelete(DeleteBehavior.Cascade);
+            entity.HasOne<User>().WithOne(u => u.Wallet).HasForeignKey<Wallet>(w => w.UserId).OnDelete(DeleteBehavior.Cascade);
 
-            entity.Property(w => w.Currency).HasColumnName("Currency").IsRequired();
-            entity.Property(w => w.Balance).HasColumnName("Balance").IsRequired().HasDefaultValue(0.00);
+            entity.Property(w => w.Currency).HasColumnName("Currency").HasMaxLength(3).IsRequired();
+            entity.Property(w => w.Balance).HasColumnName("Balance").HasDefaultValue(0m).IsRequired();
             entity.Property(w => w.Status).HasColumnName("Status").IsRequired();
             entity.Property(u => u.CreatedAt).HasColumnName("CreatedAt").HasDefaultValueSql("GETUTCDATE()");
             entity.Property(u => u.UpdatedAt).HasColumnName("UpdatedAt").HasDefaultValueSql("GETUTCDATE()");
