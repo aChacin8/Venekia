daca803889ecc64600873ae00888ee0dbeba187c
mrMcAko <alechacin08@gmail.com>
2026-01-19 19:49:41 -0600

    feat: Adding Wallet Repository Interface

---
+++ Venekia.Application/Interfaces/Finance/Wallets/IWalletRepository.cs
@@ -0,0 +1,14 @@
+﻿using Venekia.Domain.Entities.Finance.Wallets;
+using Venekia.Domain.Entities.Users;
+
+namespace Venekia.Application.Interfaces.Finance.Wallets
+{
+    public interface IWalletRepository
+    {
+        Task AddAsyncWallet(Wallet wallet);
+        Task UpdateAsync(Wallet wallet);    
+        Task DeleteAsync(Wallet wallet);
+        Task <List<Wallet>> GetWalletsByUserIdAsync(Guid userId);
+        Task<Wallet?> GetByUserIdAndCurrencyAsync(Guid userId, string currency);
+    }
+}
