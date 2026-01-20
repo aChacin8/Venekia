fea3584c1ba6ba6e681e17d637dd82014635ce93
mrMcAko <alechacin08@gmail.com>
2026-01-19 20:41:34 -0600

    feat: Create interface to wallet service

---
+++ Venekia.Application/Interfaces/Finance/Wallets/IWalletService.cs
@@ -0,0 +1,13 @@
+﻿using Venekia.Application.DTOs.Finance.Wallet;
+
+namespace Venekia.Application.Interfaces.Finance.Wallets
+{
+    public interface IWalletService
+    {
+        Task<WalletResponseDto> CreateWalletDto(Guid userId, CreateWalletDto createWalletDto);
+        Task<List<WalletResponseDto>> GetWalletByUserAsync(Guid userId);
+        Task CreditAsync(Guid userId, CreditWalletDto creditWalletDto);
+        Task DebitAsync(Guid userId, DebitWalletDto debitWalletDto);
+    }
+}
+  
