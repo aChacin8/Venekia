607b87b6433452f65da8884d485f9ba06ccfc63f
mrMcAko <alechacin08@gmail.com>
2026-01-22 19:54:41 -0600

    fix: Adjustments to Wallet settings and User relationship
    Navigation in the User relationship has been corrected.

--- Venekia.Infrastructure/Data/Finance/WalletConfig.cs
+++ Venekia.Infrastructure/Data/Finance/WalletConfig.cs
@@ -11,15 +11,15 @@ namespace Venekia.Infrastructure.Data.Finance
         {
             entity.ToTable("Wallets", "dbo");
             entity.HasKey(w => w.Id);
-            entity.Property(w => w.Id).HasColumnName("Id").ValueGeneratedNever();
+            entity.Property(w => w.Id).HasColumnName("Id").HasDefaultValueSql("NEWID()"); ;
             entity.Property(w => w.UserId).HasColumnName("UserId").IsRequired();
-            entity.HasOne<User>().WithOne(u => u.Wallet).HasForeignKey<Wallet>(w => w.UserId).OnDelete(DeleteBehavior.Cascade);
+            entity.HasOne(w => w.User).WithOne(u => u.Wallet).HasForeignKey<Wallet>(w => w.UserId).OnDelete(DeleteBehavior.Cascade);
 
             entity.Property(w => w.Currency).HasColumnName("Currency").HasMaxLength(3).IsRequired();
             entity.Property(w => w.Balance).HasColumnName("Balance").HasDefaultValue(0m).IsRequired();
             entity.Property(w => w.Status).HasColumnName("Status").IsRequired();
-            entity.Property(u => u.CreatedAt).HasColumnName("CreatedAt").HasDefaultValueSql("GETUTCDATE()");
-            entity.Property(u => u.UpdatedAt).HasColumnName("UpdatedAt").HasDefaultValueSql("GETUTCDATE()");
+            entity.Property(w => w.CreatedAt).HasColumnName("CreatedAt").HasDefaultValueSql("GETUTCDATE()");
+            entity.Property(w => w.UpdatedAt).HasColumnName("UpdatedAt").HasDefaultValueSql("GETUTCDATE()");
         }
     }
 }
