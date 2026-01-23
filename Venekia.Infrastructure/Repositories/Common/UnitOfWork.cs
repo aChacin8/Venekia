7e33af1d243efaf70aaef877edd40ef2d2d7ac13
mrMcAko <alechacin08@gmail.com>
2026-01-22 19:40:01 -0600

    feat: Creating the UnitOfWork using the interface and the Db context

---
+++ Venekia.Infrastructure/Repositories/Common/UnitOfWork.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using Venekia.Application.Interfaces.Common;
+using Venekia.Infrastructure.Data;
+
+namespace Venekia.Infrastructure.Repositories.Common
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly VenekiaDb _context;
+        private IDbContextTransaction? _currentTransaction; 
+
+        public UnitOfWork(VenekiaDb context) 
+        {
+            _context = context;
+        } 
+
+        public async Task BeginTransactionAsync()
+        {
+            if (_currentTransaction != null) return;
+
+            _currentTransaction = await _context.Database.BeginTransactionAsync(); // Start a new transaction
+        }
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+            await _currentTransaction?.CommitAsync()!;
+            await DisposeTransactionAsync();
+        }
+
+        public async Task RollbackAsync()
+        {
+            await _currentTransaction?.RollbackAsync()!; // Rollback the transaction
+            await DisposeTransactionAsync();
+        }
+        
+        private async Task DisposeTransactionAsync() // Dispose of the current transaction
+        {
+            if (_currentTransaction != null)
+            {
+                await _currentTransaction.DisposeAsync();
+                _currentTransaction = null;
+            }
+        }
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
