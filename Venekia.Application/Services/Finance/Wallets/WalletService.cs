5537a05da64e53edc05b946486ff84894a1bf016
mrMcAko <alechacin08@gmail.com>
2026-01-22 19:51:00 -0600

    feat: Adding IUnitOfWork to Begin the transactions when a user create a wallet, also implement the commits, rollback and save methods.
    Also Adding the WalletResponseDto to Credit and Debit

--- Venekia.Application/Services/Finance/Wallets/WalletService.cs
+++ Venekia.Application/Services/Finance/Wallets/WalletService.cs
@@ -1,16 +1,19 @@
 ﻿using Venekia.Application.DTOs.Finance.Wallet;
+using Venekia.Application.Interfaces.Common;
 using Venekia.Application.Interfaces.Finance.Wallets;
 using Venekia.Domain.Entities.Finance.Wallets;
 
 namespace Venekia.Application.Services.Finance.Wallets
 {
-    public class WalletService
+    public class WalletService : IWalletService
     {
         private readonly IWalletRepository _walletRepository;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public WalletService (IWalletRepository walletRepository)
+        public WalletService (IWalletRepository walletRepository, IUnitOfWork unitOfWork )
         {
             _walletRepository = walletRepository;
+            _unitOfWork = unitOfWork;
         }
 
         public async Task <WalletResponseDto> CreateWalletAsync(Guid userId, CreateWalletDto createWalletDto)
@@ -33,6 +36,63 @@ namespace Venekia.Application.Services.Finance.Wallets
             };
         }
 
+
+        public async Task<WalletResponseDto> CreditAsync (Guid userId, CreditWalletDto creditWalletDto)
+        {
+            var wallet = await _walletRepository.GetByUserIdAndCurrencyAsync(userId, creditWalletDto.Currency);
+            if (wallet == null)
+                throw new InvalidOperationException("Wallet not found for the specified user and currency.");
+
+            if (creditWalletDto.Amount <= 0)
+                throw new Exception("Amount must be greater than zero.");
+
+            wallet.Credit(creditWalletDto.Amount);
+
+            await _walletRepository.UpdateAsyncWallet(wallet);
+
+            return new WalletResponseDto
+            {
+                Id = wallet.Id,
+                UserId = wallet.UserId,
+                Balance = wallet.Balance,
+                Currency = wallet.Currency,
+                Status = wallet.Status.ToString()
+            };
+        }
+
+        public async Task<WalletResponseDto> DebitAsync (Guid userId, DebitWalletDto debitWalletDto)
+        {
+            await _unitOfWork.BeginTransactionAsync();
+
+            try
+            {
+                var wallet = await _walletRepository.GetByUserIdAndCurrencyAsync(userId, debitWalletDto.Currency);
+                if (wallet == null)
+                    throw new InvalidOperationException("Wallet not found for the specified user and currency.");
+
+                if (debitWalletDto.Amount <= 0)
+                    throw new Exception("Amount must be greater than zero.");
+
+                wallet.Debit(debitWalletDto.Amount);
+
+                await _walletRepository.UpdateAsyncWallet(wallet);
+                await _unitOfWork.CommitAsync();
+
+                return new WalletResponseDto
+                {
+                    Id = wallet.Id,
+                    UserId = wallet.UserId,
+                    Balance = wallet.Balance,
+                    Currency = wallet.Currency,
+                    Status = wallet.Status.ToString()
+                };
+            }
+            catch
+            {
+                await _unitOfWork.RollbackAsync();
+                throw;
+            }
+        }
         public async Task<List<WalletResponseDto>> GetWalletByUserAsync(Guid userId)
         {
             var existingWallet = await _walletRepository.GetWalletsByUserIdAsync(userId);
@@ -49,34 +109,6 @@ namespace Venekia.Application.Services.Finance.Wallets
             }).ToList();
 
             return wallet;
-        }
-
-        public async Task CreditAsync (Guid userId, CreditWalletDto creditWalletDto)
-        {
-            var wallet = await _walletRepository.GetByUserIdAndCurrencyAsync(userId, creditWalletDto.Currency);
-            if (wallet == null)
-                throw new InvalidOperationException("Wallet not found for the specified user and currency.");
-
-            if (creditWalletDto.Amount <= 0)
-                throw new Exception("Amount must be greater than zero.");
-
-            wallet.Credit(creditWalletDto.Amount);
-
-            await _walletRepository.UpdateAsyncWallet(wallet);
-        }
-
-        public async Task DebitAsync (Guid userId, DebitWalletDto debitWalletDto)
-        {
-            var wallet = await _walletRepository.GetByUserIdAndCurrencyAsync(userId, debitWalletDto.Currency);
-            if (wallet == null)
-                throw new Exception("Wallet not found for the specified user and currency.");
-
-            if (debitWalletDto.Amount <= 0)
-                throw new Exception("Amount must be greater than zero.");
-
-            wallet.Debit(debitWalletDto.Amount);
-
-            await _walletRepository.UpdateAsyncWallet(wallet);
         }
     }
 }
