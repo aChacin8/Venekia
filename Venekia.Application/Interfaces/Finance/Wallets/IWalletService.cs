a4cf8b701e42ac2d60188e054925e35918e80877
mrMcAko <alechacin08@gmail.com>
2026-01-22 19:48:12 -0600

    feat: Modifying the service wallet interface adding on Credit and Debit the WalletRresponseDto

--- Venekia.Application/Interfaces/Finance/Wallets/IWalletService.cs
+++ Venekia.Application/Interfaces/Finance/Wallets/IWalletService.cs
@@ -6,8 +6,8 @@ namespace Venekia.Application.Interfaces.Finance.Wallets
     {
         Task<WalletResponseDto> CreateWalletAsync(Guid userId, CreateWalletDto createWalletDto);
         Task<List<WalletResponseDto>> GetWalletByUserAsync(Guid userId);
-        Task CreditAsync(Guid userId, CreditWalletDto creditWalletDto);
-        Task DebitAsync(Guid userId, DebitWalletDto debitWalletDto);
+        Task<WalletResponseDto> CreditAsync(Guid userId, CreditWalletDto creditWalletDto);
+        Task<WalletResponseDto> DebitAsync(Guid userId, DebitWalletDto debitWalletDto);
     }
 }
   
