549abacc5008743383e1e3402a9c3e056cd3b186
mrMcAko <alechacin08@gmail.com>
2026-01-22 19:38:35 -0600

    feat: Creating the Unit of work interface to control the transaction, commits and saves

---
+++ Venekia.Application/Interfaces/Common/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace Venekia.Application.Interfaces.Common
+{
+    public interface IUnitOfWork
+    {
+        Task BeginTransactionAsync();
+        Task CommitAsync();
+        Task RollbackAsync();
+        Task SaveChangesAsync();
+    }
+}
