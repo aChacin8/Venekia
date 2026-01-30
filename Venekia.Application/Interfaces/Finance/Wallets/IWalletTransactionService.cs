a51ec6883e839b7870b0889b7e7026e7d1bf0287
mrMcAko <alechacin08@gmail.com>
2026-01-30 16:53:35 -0600

    feat: Creating the interface to the Transaction Services

---
+++ Venekia.Application/Interfaces/Finance/Wallets/IWalletTransactionService.cs
@@ -0,0 +1,11 @@
+﻿using Venekia.Domain.Entities.Finance.Wallets;
+
+namespace Venekia.Application.Interfaces.Finance.Wallets
+{
+    public interface IWalletTransactionService
+    {
+        Task RegisterCreditAsync(Wallet wallet, decimal amount, string reference);
+        Task RegisterDebitAsync (Wallet wallet, decimal amount, string reference);
+        Task<List<WalletTransaction>>  GetTransactionHistoryAsync(Guid walletId);
+    }
+}
