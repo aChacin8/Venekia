7e24e403a6d98653d8eb39284c56a603e5d0be28
mrMcAko <alechacin08@gmail.com>
2026-01-30 15:19:06 -0600

    feat: Creating the repository to Add, and get Transactions, also the method to GetTje transactions by walletsId

---
+++ Venekia.Infrastructure/Repositories/Finance/Wallets/WalletTransactionRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Venekia.Application.Interfaces.Finance.Wallets;
+using Venekia.Domain.Entities.Finance.Wallets;
+using Venekia.Infrastructure.Data;
+
+namespace Venekia.Infrastructure.Repositories.Finance.Wallets
+{
+    public class WalletTransactionRepository : IWalletTransactionsRepository
+    {
+        private readonly VenekiaDb _context;
+
+        public WalletTransactionRepository(VenekiaDb context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsyncTransaction(WalletTransaction transaction)
+        {
+            await _context.WTransactions.AddAsync(transaction);
+        }
+
+        public async Task <List<WalletTransaction>> GetTransactionsAsync(Guid walletId)
+        {
+            return await _context.WTransactions.Where(t => t.WalletId == walletId).OrderByDescending(t => t.CreatedAt).ToListAsync(); //Se optiene la lista de transacciones de una billetera ordenadas por fecha de creacion descendente
+        }
+
+        public async Task <WalletTransaction?> GetTransactionByIdAsync(Guid transactionId)
+        {
+            return await _context.WTransactions.FirstOrDefaultAsync(t => t.Id == transactionId);
+        }
+    }
+}
