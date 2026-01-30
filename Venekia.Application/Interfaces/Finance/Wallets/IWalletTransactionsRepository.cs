6a27ae21b8eee3a8a3b6c2316c849895fb5630d4
mrMcAko <alechacin08@gmail.com>
2026-01-30 15:16:58 -0600

    feat: Creating the Interface to WalletTransactionRepository

---
+++ Venekia.Application/Interfaces/Finance/Wallets/IWalletTransactionsRepository.cs
@@ -0,0 +1,11 @@
+﻿using Venekia.Domain.Entities.Finance.Wallets;
+
+namespace Venekia.Application.Interfaces.Finance.Wallets
+{
+    public interface IWalletTransactionsRepository
+    {
+        Task AddAsyncTransaction(WalletTransaction transaction);
+        Task<List<WalletTransaction>> GetTransactionsAsync(Guid walletId);
+        Task<WalletTransaction?> GetTransactionByIdAsync(Guid transactionId);
+    }
+}
