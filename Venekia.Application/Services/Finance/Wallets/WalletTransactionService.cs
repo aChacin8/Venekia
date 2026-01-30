f15d7aad992ab079545a006ab94b79cd8c3fcd55
mrMcAko <alechacin08@gmail.com>
2026-01-30 16:56:11 -0600

    feat: The WalletTransactionService class is created, which implements the IWalletTransactionService interface, allowing credit and debit transactions to be recorded in wallets, as well as querying the transaction history. Use IWalletTransactionsRepository for data persistence and retrieval.

---
+++ Venekia.Application/Services/Finance/Wallets/WalletTransactionService.cs
@@ -0,0 +1,60 @@
+﻿using Venekia.Application.Interfaces.Finance.Wallets;
+using Venekia.Domain.Entities.Finance.Wallets;
+
+namespace Venekia.Application.Services.Finance.Wallets
+{
+    public class WalletTransactionService : IWalletTransactionService
+    {
+        private readonly IWalletTransactionsRepository _transactionsRepository;
+
+        public WalletTransactionService (IWalletTransactionsRepository transactionsRepository)
+        {
+            _transactionsRepository = transactionsRepository;
+        }
+
+        public async Task RegisterCreditAsync (Wallet wallet, decimal amount, string reference)
+        {
+            var balanceBefore = wallet.Balance;
+
+            wallet.Credit(amount);
+
+            var balanceAfter = wallet.Balance;
+
+            var transaction = new WalletTransaction(
+                wallet.Id,
+                WalletTransaction.TransactionType.Credit,
+                amount,
+                balanceBefore,
+                balanceAfter,
+                reference
+                );
+
+            await _transactionsRepository.AddAsyncTransaction(transaction);
+        }
+
+        public async Task RegisterDebitAsync (Wallet wallet, decimal amount, string reference)
+        {
+            var balanceBefore = wallet.Balance;
+
+            wallet.Debit(amount);
+
+            var balanceAfter = wallet.Balance;
+
+            var transaction = new WalletTransaction(
+                wallet.Id,
+                WalletTransaction.TransactionType.Debit,
+                amount,
+                balanceBefore,
+                balanceAfter,
+                reference
+                );
+
+            await _transactionsRepository.AddAsyncTransaction(transaction);
+        }
+
+        public async Task <List<WalletTransaction>> GetTransactionHistoryAsync(Guid walletId)
+        {
+            return await _transactionsRepository.GetTransactionsAsync(walletId);
+        }
+    }
+}
