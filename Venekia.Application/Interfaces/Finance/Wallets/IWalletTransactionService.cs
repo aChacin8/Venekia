42fd10f51e967d9b7f467343caf4b1d03c8b761d
mrMcAko <alechacin08@gmail.com>
2026-01-30 19:04:40 -0600

    fix: Modifying the Interface adding on Credit and Debir methods the balanceBefore and balanceAfter from WalletService

--- Venekia.Application/Interfaces/Finance/Wallets/IWalletTransactionService.cs
+++ Venekia.Application/Interfaces/Finance/Wallets/IWalletTransactionService.cs
@@ -4,8 +4,8 @@ namespace Venekia.Application.Interfaces.Finance.Wallets
 {
     public interface IWalletTransactionService
     {
-        Task RegisterCreditAsync(Wallet wallet, decimal amount, string reference);
-        Task RegisterDebitAsync (Wallet wallet, decimal amount, string reference);
+        Task RegisterCreditAsync(Wallet wallet, decimal amount, decimal balanceBefore, decimal balanceAfter, string reference);
+        Task RegisterDebitAsync (Wallet wallet, decimal amount, decimal balanceBefore, decimal balanceAfter, string reference);
         Task<List<WalletTransaction>>  GetTransactionHistoryAsync(Guid walletId);
     }
 }
