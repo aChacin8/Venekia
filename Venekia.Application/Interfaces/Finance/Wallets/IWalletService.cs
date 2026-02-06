1b332f1c2c7fd889b6745600e22cfd102aaa4d78
mrMcAko <alechacin08@gmail.com>
2026-02-05 20:09:10 -0600

    feat: extend IWalletService with wallet and transaction retrieval methods

--- Venekia.Application/Interfaces/Finance/Wallets/IWalletService.cs
+++ Venekia.Application/Interfaces/Finance/Wallets/IWalletService.cs
@@ -5,9 +5,11 @@ namespace Venekia.Application.Interfaces.Finance.Wallets
     public interface IWalletService
     {
         Task<WalletResponseDto> CreateWalletAsync(Guid userId, CreateWalletDto createWalletDto);
-        Task<List<WalletResponseDto>> GetWalletByUserAsync(Guid userId);
         Task<WalletResponseDto> CreditAsync(Guid userId, CreditWalletDto creditWalletDto);
         Task<WalletResponseDto> DebitAsync(Guid userId, DebitWalletDto debitWalletDto);
+        Task<List<WalletResponseDto>> GetWalletByUserAsync(Guid userId);
+        Task<List<WalletTransactionResponseDto>> GetTransactionHistoryAsync(Guid userId, Guid walletId);
+        Task<WalletTransactionResponseDto> GetTransactionByIdAsync(Guid userId, Guid walletId, Guid transactionId);
     }
 }
   
